dd5c577423da90c4889d4edca5b67b055aaaaf31
clio001 <johnwoitkowitz@hotmail.com>
2022-04-30 23:22:56 +0200

    added btn removal

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 async function getData() {
   let response = await fetch(
-    'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=("Bennhold")&fq=news_desk:("Foreign")&api-key=jbIYjBeDQwCAfrWak0psVqCGshuSaU2y'
+    'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=("antarctica")&fq=news_desk:("Foreign")&api-key=jbIYjBeDQwCAfrWak0psVqCGshuSaU2y'
   );
-  let myData = response.json;
+  let myData = await response.json();
   printResult(myData);
+  console.log(myData);
+}
+
+function printResult(myData) {
+  document.querySelector("#api-title").textContent =
+    myData.response.docs[0].headline.main;
+
+  document.querySelector("#api-byline").textContent =
+    myData.response.docs[0].byline.original;
+
+  document.querySelector("#api-abstract").textContent =
+    myData.response.docs[0].abstract;
+
+  let urlImg =
+    "https://static01.nyt.com/" + myData.response.docs[0].multimedia[0].url;
+  document.querySelector("#api-img").setAttribute("src", urlImg);
+
+  document.querySelector("#btn").remove();
 }
