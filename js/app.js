a3a316d44583393f3a8322c1554d5bd7252af2e8
clio001 <johnwoitkowitz@hotmail.com>
2022-04-30 22:40:26 +0200

    added fetch url

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 async function getData() {
-  let response = await fetch();
+  let response = await fetch(
+    'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=("Bennhold")&fq=news_desk:("Foreign")&api-key=jbIYjBeDQwCAfrWak0psVqCGshuSaU2y'
+  );
   let myData = response.json;
   printResult(myData);
 }
