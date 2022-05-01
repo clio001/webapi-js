54c95f677d227a332d71edf88972c0a523ebe5fd
clio001 <johnwoitkowitz@hotmail.com>
2022-05-01 18:30:33 +0200

    Added date and section to card. Edited control panel.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 async function getData() {
   let response = await fetch(
-    'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Arctic&fq=news_desk;("Business")&api-key=jbIYjBeDQwCAfrWak0psVqCGshuSaU2y'
+    'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Antarctica&fq=news_desk:("Foreign")&api-key=jbIYjBeDQwCAfrWak0psVqCGshuSaU2y'
   );
   let myData = await response.json();
   printResult(myData);
@@ -17,7 +17,28 @@ function printResult(myData) {
   document.querySelector("#api-abstract").textContent =
     myData.response.docs[0].abstract;
 
+  let pubdate = new Date(myData.response.docs[0].pub_date);
+  articlePubdate = pubdate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  document.querySelector("#api-date").textContent = articlePubdate;
+
+  document.querySelector("#api-section").textContent =
+    myData.response.docs[0].news_desk;
+
   let urlImg =
     "https://static01.nyt.com/" + myData.response.docs[0].multimedia[0].url;
   document.querySelector("#api-img").setAttribute("src", urlImg);
 }
+
+/* DISABLE REGION DROPDOWN WHEN ANTARCTICA SELECTED
+
+let antarcticaRadio = document.querySelector("#inputRadioAntarctica");
+antarcticaRadio.addEventListener("click", () => {
+  if (antarcticaRadio.checked) {
+    document.querySelector("#inputRegion").disabled = true;
+  }
+}); */
